945b74e90dcc080cb1d02a7f70af5bbe4d8e03ba
Oleksandr Filippov <alexandrph1997@hotmail.com>
2019-11-01 17:07:47 +0100

    Created project

---
+++ ios/diet-in-a-box/diet-in-a-box/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  diet-in-a-box
+//
+//  Created by Oleksandr Filippov on 28/10/2019.
+//  Copyright © 2019 Oleksandr Filippov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
